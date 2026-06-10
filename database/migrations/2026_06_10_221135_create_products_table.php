f6cbfcb300cbfe51abf82fb06574cf55e6f9d504
erdemkoyuncuoglu1 <erdemkoyuncuoglu.dev@gmail.com>
2026-06-11 01:19:53 +0300

    Created products database structure

--- database/migrations/2026_06_10_221135_create_products_table.php
+++ database/migrations/2026_06_10_221135_create_products_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+             $table->id();
+             $table->string('name');
+    	     $table->text('description')->nullable();
+             $table->decimal('price', 10, 2);
+             $table->integer('stock')->default(0);
+             $table->timestamps();
+
         });
     }
 
