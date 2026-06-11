983b968a2feabaa446a93e8fdece7a737495f3c3
erdemkoyuncuoglu1 <erdemkoyuncuoglu.dev@gmail.com>
2026-06-11 20:10:25 +0300

    Added product creation form

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Product</title>
+</head>
+<body>
+
+<h1>Create Product</h1>
+
+<form>
+    <label>Product Name:</label><br>
+    <input type="text"><br><br>
+
+    <label>Description:</label><br>
+    <textarea></textarea><br><br>
+
+    <label>Price:</label><br>
+    <input type="number"><br><br>
+
+    <label>Stock:</label><br>
+    <input type="number"><br><br>
+
+    <button type="submit">Save Product</button>
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/products', function () {
     return view('products.index');
 });
+
+Route::get('/products/create', [ProductController::class, 'create']);
